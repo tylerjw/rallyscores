111d87e3feaa449bfbba26271c47351615e63584
Tyler Weaver <tweaver@lgsinnovations.com>
2017-07-29 01:39:26 -0600

    reduced the cache time

--- timecontrol/timecontrol/settings.py
+++ timecontrol/timecontrol/settings.py
@@ -84,7 +84,7 @@ AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
 # Enable and configure HTTP caching (disabled by default)
 # See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
 HTTPCACHE_ENABLED = True
-HTTPCACHE_EXPIRATION_SECS = 86400 # one day
+HTTPCACHE_EXPIRATION_SECS = 9600 # 2 minutes
 HTTPCACHE_DIR = 'httpcache'
 HTTPCACHE_IGNORE_HTTP_CODES = []
 HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
